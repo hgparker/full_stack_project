a98124617e049ea6e1ec7271d2685131d8824340
hgparker <haroldgparker@gmail.com>
2020-10-13 09:12:16 -0700

    Set up and initialized webpack

---
+++ babka_overflow/frontend/entry.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Just a test to see if webpack is working correctly.");
+    const root = document.getElementById("root");
+    ReactDOM.render(<h1> Good news...React is working!</h1>, root);
+})
