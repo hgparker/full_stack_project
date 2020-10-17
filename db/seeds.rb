72dad4d3c6c05345ed5aa683ba7c783ece9b254e
hgparker <haroldgparker@gmail.com>
2020-10-17 15:15:03 -0700

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,14 @@
 User.destroy_all
 User.create(username: "Demo", email: "demodocus@demo.com", password: "demodemo")
 
-User.create(username: "thales", email: "thales@thales.com", password: "password")
-User.create(username: "parmeniPLEASE", email: "parm@parm.com", password: "password")
-User.create(username: "heraclitus", email: "hera@hera.com", password: "password")
-User.create(username: "anaxagoras", email: "anax@anax.com", password: "password")
+berivan = User.create(username: "Berivan", email: "berivan@berivan.com", password: "password")
+abdullah = User.create(username: "Abdullah", email: "apo@apo.com", password: "password")
+sakine = User.create(username: "Sakine", email: "sakine@sakine.com", password: "password")
+selahattin = User.create(username: "Selahattin", email: "selo@selo.com", password: "password")
+
+Question.destroy_all
+
+Question.create(author_id: berivan.id , title: "What is the Morris traversal?", body: "I understand how to traverse a binary tree in in-order, pre-order, and post-order, and I know how to do this either iteratively or recursively. What's the Morris traversal, and how does that fit into the above? What's special about it?")
+Question.create(author_id: abdullah.id, title: "How do you actually solve the Traveling Salesman Problem?", body: "A traveling salesman must visit each of n cities once. Given the distance between every pair of cities, find the minimum path. Help! How do I even begin to solve this? At a loss here. Is this problem hopeless?")
+Question.create(author_id: sakine.id, title: "What are some fast ways to search for primes?", body: "To examine a particular number for primality, we can simply test whether it has any factors between 2 and itself minus 1. Still, this seems pretty slow. How can we search faster?")
+Question.create(author_id: selahattin.id, title: "ELI5 Suffix Tree", body: "Can somebody please explain to me in the simplest possible terms what a suffix tree is?")
