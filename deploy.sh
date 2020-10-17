1e39c6ec3f82d0a4401bda761fb6a2eab8a1ff87
hgparker <haroldgparker@gmail.com>
2020-10-17 15:24:37 -0700

    Heroku test

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 
-bundle exec rake assets:precompile RAILS_ENV=production
+# bundle exec rake assets:precompile RAILS_ENV=production
 git add .
-git commit -m \"$1\"
-git push origin main
-git push heroku main
-heroku run rails db:migrate
-heroku run rails db:seed
+echo git commit -m \"$1\"
+# git push origin main
+# git push heroku main
+# heroku run rails db:migrate
+# heroku run rails db:seed
