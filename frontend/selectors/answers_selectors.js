8e292ac7b3206edf90113f1353cbd30a3551b218
hgparker <haroldgparker@gmail.com>
2020-10-20 22:38:53 -0700

    Fix wandering answer problem

--- frontend/selectors/answers_selectors.js
+++ frontend/selectors/answers_selectors.js
@@ -3,5 +3,5 @@
 
 export const selectAnswers = (state, questionId) => {
     return Object.values(state.entities.answers)
-        .filter((answer) => answer.question_id !== questionId);
+        .filter(answer => answer.question_id == questionId);
 } 
