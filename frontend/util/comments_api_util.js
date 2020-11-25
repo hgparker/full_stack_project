3b49d69fec8a4d143296de7631dbac443f5d1192
hgparker <haroldgparker@gmail.com>
2020-11-25 09:41:08 -0800

    Add comment api_util in frontend

---
+++ frontend/util/comments_api_util.js
@@ -0,0 +1,13 @@
+
+
+export const postComment = (comment) => {
+  return $.ajax({url: `/api/comments`, method: "POST", data: {comment}});
+}
+
+export const updateComment = (comment) => {
+  return $.ajax({url: `/api/comments/${comment.id}`, method: "PATCH", data: {comment}});
+}
+
+export const deleteComment = (commentId) => {
+  return $.ajax({url: `/api/comments/${commentId}`, method: "DELETE"});
+}
