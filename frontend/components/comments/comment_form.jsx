5144ef9526f549de6e826e520722e2f0bdf61a65
hgparker <haroldgparker@gmail.com>
2020-11-25 16:36:35 -0800

    Add skeleton for CommentForm and container

---
+++ frontend/components/comments/comment_form.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+// import { Redirect } from 'react-router-dom';
+// import Errors from '../errors';
+
+class CommentForm extends React.Component {
+  render() {
+    return (
+      <div>
+        This is where the comment form will appear...
+      </div>
+    );
+  }
+}
+
+
+// class QuestionForm extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = this.props.question;
+
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//         this.handleChange = this.handleChange.bind(this);
+//     }
+
+//     componentDidMount() {
+//         this.props.clearQuestionErrors();
+//     }
+
+//     handleSubmit(e) {
+//         e.preventDefault();
+//         this.props.submit(this.state)
+//             .then(action => {
+//                 this.props.history.push(`/questions/${Object.values(action.payload.questions)[0].id}`)
+//             });
+//     }
+
+//     handleChange(field) {
+//         return (e) => {
+//             this.setState({
+//                 [field] : e.currentTarget.value
+//             });
+//         }
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <form className="QuestionFormBox1"
+//                     onSubmit={this.handleSubmit}
+//                 >
+//                     <div className="QuestionFormBox">
+//                     <label>Title</label>
+//                         <input
+//                             className="QuestionFormTitle"
+//                             type="text"
+//                             row="60"
+//                             value={this.state.title}
+//                             onChange={this.handleChange("title")}
+//                         />
+//                     </div>
+//                     <div className="QuestionFormBox">
+//                     <label>Body</label>
+//                         <textarea
+//                             className="QuestionFormBody"
+//                             cols="60"
+//                             value={this.state.body}
+//                             onChange={this.handleChange("body")}
+//                         />
+//                     </div>
+//                     <button
+//                         className="ButtonStyle1"
+//                         value="submit"
+//                     >{this.props.formType} </button>
+//                     <Errors errors={this.props.errors}/>
+//                     </form> 
+
+//             </div>
+//         )
+//     }
+// }
+
+export default CommentForm;
---
+++ frontend/components/comments/comment_form_container.js
@@ -0,0 +1,30 @@
+import {connect} from 'react-redux';
+import CommentForm from './comment_form';
+// import {postQuestion, clearQuestionErrors} from '../../actions/question_actions';
+// import {questionErrors} from '../../selectors/errors_selectors'
+// import { currentUser } from '../../selectors/auth_selectors';
+
+const mSTP = (state) => {
+    return {}
+//         errors: questionErrors(state),
+//         formType: "Post your question",
+//         question: {
+//             title: "",
+//             body: "",
+//             author_id: currentUser(state)
+//         }
+//     };
+}
+
+// const mDTP = (dispatch) => {
+//     return {
+//         submit: (question) => {
+//             return dispatch(postQuestion(question))
+//         },
+//         clearQuestionErrors: () => {
+//             dispatch(clearQuestionErrors())
+//         }
+//     }
+// }
+
+export default connect(mSTP, null)(CommentForm);
