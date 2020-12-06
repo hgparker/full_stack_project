6c3ad7b2f4c33567368cde17a4f599ae67d1a784
hgparker <haroldgparker@gmail.com>
2020-12-05 20:58:48 -0800

    Input basic lower structure on splash page

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -26,12 +26,32 @@ const Splash = (props) => {
           For recruiters, by a jobseeker
         </div>
 
-        <div className="SplashOrangeBar">
+        <div className="SplashOrangeBar"> </div>
+
+        <div className="SplashAbout">
+        Babka Overflow is a full stack clone of Stack Overflow.
+        It offers user account authentication and creation, the ability to post and edit questions,
+        the ability to post and edit answers to those questions,
+        the ability to post and edit comments on answers,
+        and the ability to vote on questions and answers.
+        Don't take this &lt;div&gt;'s word for it, though. Click around for yourself and see!
+        Below is some more information about the specific technologies used in the site.        
 
         </div>
       </div>
       <div className="SplashLowerSection">
-        lower
+        <div className="SplashTech1">
+
+        </div>
+        <div className="SplashTech2">
+
+        </div>
+        <div className="SplashTech3">
+
+        </div>
+        <div className="SplashTech4">
+
+        </div>
       </div>
     </div>
   );
